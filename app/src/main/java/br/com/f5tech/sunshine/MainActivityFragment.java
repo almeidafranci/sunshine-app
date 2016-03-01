d4bc6f3d2d5efcedae5c323fbe29e1ab88f84c58
Francinaldo Almeida <fap_sb@globomail.com>
2016-02-29 20:49:47 -0600

    Hook the adapter up to the ListView

--- app/src/main/java/br/com/f5tech/sunshine/MainActivityFragment.java
+++ app/src/main/java/br/com/f5tech/sunshine/MainActivityFragment.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -35,6 +38,22 @@ public class MainActivityFragment extends Fragment {
         };
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        mForecastAdapter = new ArrayAdapter<String>(
+            getActivity(), // The current context (this activity)
+            R.layout.list_item_forecast, // The name of the layout ID.
+            R.id.list_item_forecast_textview, // The ID of the textview to populate.
+            weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
