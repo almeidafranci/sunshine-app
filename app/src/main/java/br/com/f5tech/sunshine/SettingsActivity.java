41627869c92f128bb9b83f0246c0de5e972df8df
Francinaldo Almeida <fap_sb@globomail.com>
2016-04-08 18:53:14 -0500

    Add Units setting

--- app/src/main/java/br/com/f5tech/sunshine/SettingsActivity.java
+++ app/src/main/java/br/com/f5tech/sunshine/SettingsActivity.java
@@ -206,6 +206,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         // their values. When their values change, their summaries are updated
         // to reflect the new value, per the Android Design guidelines.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_temperature_key)));
         bindPreferenceSummaryToValue(findPreference("example_text"));
         bindPreferenceSummaryToValue(findPreference("example_list"));
         bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
